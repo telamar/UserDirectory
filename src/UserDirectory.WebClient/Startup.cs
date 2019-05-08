844b4d8f912a4292198efed2d89a64dc4d0a8beb
Telamar <telamarfolen@gmail.com>
2019-05-08 15:57:21 +0300

    Added empty webclient project

---
+++ src/UserDirectory.WebClient/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserDirectory.WebClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+}
