e083afb0236c72a385908a7c088e9705fd1c2413
Telamar <telamarfolen@gmail.com>
2019-05-10 15:37:31 +0300

    Added angular support in WebClient project

---
+++ src/UserDirectory.WebClient/ClientApp/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: ``
+})
+export class AppComponent {}
--- src/UserDirectory.WebClient/Startup.cs
+++ src/UserDirectory.WebClient/Startup.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UserDirectory.WebClient
 {
     public class Startup
     {
-        private const string SpaFilePath = "src";
+        private const string SpaFilePath = "ClientApp";
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddNodeServices(options =>
+            {
+                options.ProjectPath = Path.Combine(Directory.GetCurrentDirectory(), SpaFilePath);
+            });
+
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = SpaFilePath;
@@ -21,6 +28,12 @@ namespace UserDirectory.WebClient
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseWebpackDevMiddleware(
+                    new WebpackDevMiddlewareOptions
+                    {
+                        HotModuleReplacement = true
+                    });
             }
 
             app.UseStaticFiles();
